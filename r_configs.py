1fdf41fe7596dd6237775fe61da65f1c9e79935b
sgzd <2815861964@qq.com>
2023-05-05 17:33:54 +0800

    变更

--- r_configs.py
+++ r_configs.py
@@ -1,12 +1,7 @@
 CONFIGS = [
     {
-        "name":"work",
-        "time":'17:00:00',
-        "folder_path":'E:\迅雷下载\Python代码\Python_For_Beginners\mon'
-    },
-    {
         "name":"game",
-        "time":'16:00:00',
+        "time":'21:00:00',
         "folder_path":'E:\迅雷下载\Python代码\Python_For_Beginners\eve'
     }
 ]
--- zhuangshiqi.py
+++ zhuangshiqi.py
@@ -1,18 +1,17 @@
 import time
 def timeif(f):
-
     def wrapper(x):
+        print("测试")
         start = time.perf_counter()
         ret = f(x)
         print(time.perf_counter()-start)
         return ret
     return  wrapper
-
 @timeif
 def fuc(x):
     time.sleep(x)
 @timeif
 def fc2(y):
-    return y*y
+    print(y*y)
 fuc(1)
 fc2(9)
